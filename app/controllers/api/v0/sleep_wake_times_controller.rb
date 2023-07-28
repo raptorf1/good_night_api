5053895acf82063c6b6601971b37034b2b620cfa
raptorf1 <raptor_f1@hotmail.com>
2023-07-28 15:06:23 +0300

    Creates the create action to log sleep time. Adds relevant spec

--- app/controllers/api/v0/sleep_wake_times_controller.rb
+++ app/controllers/api/v0/sleep_wake_times_controller.rb
@@ -2,4 +2,13 @@ class Api::V0::SleepWakeTimesController < ApplicationController
   def index
     render json: SleepWakeTime.fetch_all_sorted_by_created_at, each_serializer: SleepWakeTimes::Serializer, status: 200
   end
+
+  def create
+    record_to_create = SleepWakeTime.create(user_id: params[:user_id], sleep: Time.now)
+    if !record_to_create.persisted?
+      render json: { errors: record_to_create.errors.full_messages }, status: 400 and return
+    end
+
+    render json: { message: "Success!" }, status: 200
+  end
 end
---
+++ spec/requests/api/v0/sleep_wake_times/create_spec.rb
@@ -0,0 +1,47 @@
+RSpec.describe "POST /api/v0/sleep_wake_times", type: :request do
+  let(:user) { FactoryBot.create(:user) }
+
+  describe "succesfully" do
+    before { post "/api/v0/sleep_wake_times", params: { user_id: user.id } }
+
+    it "with 200 status" do
+      expect(response.status).to eq 200
+    end
+
+    it "with correct message" do
+      expect(json_response["message"]).to eq "Success!"
+    end
+
+    it "with correct number of records saved in the DB" do
+      expect(SleepWakeTime.all.count).to eq 1
+    end
+
+    it "with correct user for the created record of the DB" do
+      expect(SleepWakeTime.all.first.user.id).to eq user.id
+    end
+
+    it "with sleep datetime of the created record populated" do
+      expect(SleepWakeTime.all.first.sleep).to_not eq nil
+    end
+  end
+
+  describe "unsuccesfully" do
+    before { post "/api/v0/sleep_wake_times", params: { user_id: 15_000 } }
+
+    it "with 400 status" do
+      expect(response.status).to eq 400
+    end
+
+    it "with no records created in the DB" do
+      expect(SleepWakeTime.all.count).to eq 0
+    end
+
+    it "with correct number of errors" do
+      expect(json_response["errors"].count).to eq 1
+    end
+
+    it "with correct error message" do
+      expect(json_response["errors"].first).to eq "User must exist"
+    end
+  end
+end
