6d147efee5d089417f4ca430a66410aa3a3742d6
raptorf1 <raptor_f1@hotmail.com>
2023-07-29 17:54:22 +0300

    Removes unused routes for existing controllers. All app tests passing.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v0 do
-      resources :users, only: %i[index show create update destroy]
-      resources :sleep_wake_times, only: %i[index show create update destroy]
+      resources :users, only: %i[index show create destroy]
+      resources :sleep_wake_times, only: %i[index create update]
     end
   end
 end
