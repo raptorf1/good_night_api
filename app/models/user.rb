68d8f706ace8798049c31da8572a8904b41f14b3
raptorf1 <raptor_f1@hotmail.com>
2023-07-31 14:54:53 +0300

    Returns friends sleep records when requesting info for a specific user

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,27 @@ class User < ApplicationRecord
   has_many :follow_association_2, class_name: "FollowAssociation", foreign_key: "user_to_follow_id", dependent: :destroy
 
   validates :name, presence: true
+
+  def self.one_week_sleep_records_of_user_and_following_sorted_by_difference(user_id)
+    friends_ids = []
+    sleep_records = []
+
+    sleep_records.push(SleepWakeTime.where(user_id: user_id))
+
+    existing_associations = FollowAssociation.where(requested_by_user_id: user_id).select(:user_to_follow_id)
+    if !existing_associations.empty?
+      existing_associations.each { |association| friends_ids.push(association.user_to_follow_id) }
+    end
+
+    friends_ids.each { |id| sleep_records.push(SleepWakeTime.where(user_id: id)) }
+
+    if !sleep_records.empty?
+      sleep_records
+        .flatten
+        .reject { |sleep_record| sleep_record.difference.nil? }
+        .reject { |sleep_record| sleep_record.wake < (DateTime.now.utc - 1.week) }
+        .sort_by { |sleep_record| sleep_record.difference }
+        .reverse
+    end
+  end
 end
--- app/serializers/users/show_serializer.rb
+++ app/serializers/users/show_serializer.rb
@@ -2,6 +2,9 @@ class Users::ShowSerializer < ActiveModel::Serializer
   attributes :id, :name, :sleep_records
 
   def sleep_records
-    SleepWakeTime.where(user_id: object.id).select(:id, :sleep, :wake, :difference)
+    ActiveModelSerializers::SerializableResource.new(
+      User.one_week_sleep_records_of_user_and_following_sorted_by_difference(object.id),
+      each_serializer: SleepWakeTimes::Serializer
+    )
   end
 end
